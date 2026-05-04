3bd28ae770b0f8552ac69cba9919894142c20bd4
shaniwaris80-crypto <shaniwaris80@gmail.com>
2026-05-04 21:02:33 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+window.FACTURA_CONFIG = {
+  companyId: "aw",
+  apiKey: "AIzaSyB_6p9FWV362ekLKDcX_trlOaui9aqafUI",
+  databaseURL: "https://awv9-f3e14-default-rtdb.europe-west1.firebasedatabase.app",
+  appName: "FACTURA AW",
+  company: {
+    name: "Mohammad Arslan Waris",
+    nif: "X6389988J",
+    address: "Calle San Pablo 17, 09003 Burgos",
+    phone: "631 667 893",
+    email: "shaniwaris80@gmail.com"
+  },
+  internalPin: "1234"
+};
